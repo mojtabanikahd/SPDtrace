f755492519c2b809d70dcd1c01909c9d66af209b
mojtaba nikahd <mojtabanikahd@gmail.com>
2025-09-24 21:19:39 +0330

    Docs/NAMESPACE: add package-level roxygen, useDynLib, and stats imports; regenerate man

---
+++ R/SPDtrace-package.R
@@ -0,0 +1,9 @@
+#' SPDtrace Package
+#'
+#' Efficient Learning of Differential Networks in Multi-Source Non-Paranormal Graphical Models.
+#'
+#' @keywords internal
+#' @useDynLib SPDtrace, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats cor fisher.test median
+"_PACKAGE"
